d16da806e431bdcafd503e8965459dc420071525
박영남 <parkyn007@gmail.com>
2025-10-12 15:30:17 +0900

    work

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 const csRouter = require('./routes/cs');
 
-// 정적 파일 서빙: /cs 경로에서 제공
-app.use('/cs', express.static(__dirname));
 app.use(cookieParser());
-
 app.use('/cs', csRouter);
+app.use('/cs/static', express.static(path.join(__dirname, 'views')));
 
 app.listen(3003, () => {
     console.log('✅ 서버 실행 중: http://localhost:3003');
